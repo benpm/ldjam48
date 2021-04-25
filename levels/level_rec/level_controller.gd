fb72ec4be3497528f88c89d177d5bde907c9ca2a
benpm <schwalegos@gmail.com>
2021-04-25 01:12:42 -0600

    recursion level setting tiles now

--- levels/level_rec/level_controller.gd
+++ levels/level_rec/level_controller.gd
@@ -3,6 +3,9 @@ extends Node
 var pressed: Array
 var combo_correct: bool = false
 
+const display_tilemap_coords := [Vector2(13, 10), Vector2(15, 10), Vector2(17, 10)]
+const display_tileset_coords := [Vector2(8, 0), Vector2(9, 0), Vector2(10, 0), Vector2(11, 0)]
+
 func _ready():
 	zone_change()
 
@@ -17,10 +20,18 @@ func press_number(who: Node2D, data: Dictionary):
 		return
 	pressed.append(data["number"])
 	print_debug("level controller pressed: ", pressed)
+	var tilemap: TileMap = $"/root/scene".get_node("TileMap")
+	for i in range(3):
+		var value := 0
+		if i < pressed.size():
+			value = pressed[i]
+		tilemap.set_cell(
+			display_tilemap_coords[i].x,
+			display_tilemap_coords[i].y, 0, false, false, false,
+			display_tileset_coords[value])
 	if pressed.size() == 3:
 		if pressed[0] == 1 and pressed[1] == 2 and pressed[2] == 3:
 			print_debug("WEINER")
 			combo_correct = true
 		else:
 			pressed.clear()
-	var tilemap: TileMap = $"/root/scene".get_node("TileMap")
